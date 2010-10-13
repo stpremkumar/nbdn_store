b674f6bb56b87e4c7836dbcdf0a66d796aacb73f
JB Osborne <jozbone@live.com>
2010-10-13 13:51:58 -0400

    front controller

--- product/nothinbutdotnetstore/web/infrastructure/DefaultFrontController.cs
+++ product/nothinbutdotnetstore/web/infrastructure/DefaultFrontController.cs
@@ -4,9 +4,16 @@ namespace nothinbutdotnetstore.web.infrastructure
 {
     public class DefaultFrontController : FrontController
     {
+        RequestCommandRegistry registry;
+
+        public DefaultFrontController(RequestCommandRegistry registry)
+        {
+            this.registry = registry;
+        }
+
         public void process(Request request)
         {
-            throw new NotImplementedException();
+            registry.get_the_command_that_can_process(request).process(request);
         }
     }
 }
