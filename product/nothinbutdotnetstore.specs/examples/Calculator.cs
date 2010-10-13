8c83841bf970ce47bd6ee1e586d7334a19b96677
Jean-Paul S. Boodhoo <jp@jpboodhoo.com>
2010-10-13 10:25:39 -0600

    Moved the Calculator to the specs project

--- product/nothinbutdotnetstore.specs/examples/Calculator.cs
+++ product/nothinbutdotnetstore.specs/examples/Calculator.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Security;
 using System.Threading;
 
-namespace nothinbutdotnetstore
+namespace nothinbutdotnetstore.specs.examples
 {
     public class Calculator
     {
